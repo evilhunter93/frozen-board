61b4a3931f7e580f56812e0b9006e3f6f25828f5
KOOL93 <KOOL_music@hotmail.com>
2025-12-21 22:04:39 +0100

    Reworking drag into forward and side

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,14 +7,13 @@ extends CharacterBody3D
 @export var jump_charge_time := 1.0
 @export var turn_speed := 1.0
 @export var min_speed := 4.0
-@export var drag := 1.0
+@export var forward_drag := 0.1
 @export var side_drag := 3.0
 
 @onready var character_mesh: MeshInstance3D = $CharacterMesh
 
 const SPEED = 5.0
 var jump_charge := 0.0
-var was_in_air = true
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor() or is_on_wall():
@@ -24,7 +23,6 @@ func _physics_process(delta: float) -> void:
 
 	# Add the gravity and handle acceleration
 	if not is_on_wall():
-		was_in_air = true
 		velocity += get_gravity() * delta
 		if -velocity.y > max_fall_speed:
 			velocity.y = -max_fall_speed
@@ -42,12 +40,20 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
+func _align_with_surface(xform: Transform3D):
+	var normal = Vector3.ZERO
+	if is_on_floor():
+		normal = get_floor_normal()
+	else:
+		normal = get_wall_normal()
+
+	xform.basis.y = normal
+	xform.basis.x = -xform.basis.z.cross(normal)
+	xform.basis = xform.basis.orthonormalized()
+	return xform
+
 func _slide_velocity(delta: float) -> void:
 	var wall_normal = get_wall_normal()
-
-	if was_in_air:
-		was_in_air = false
-		velocity = velocity.slide(wall_normal)
 
 	var gravity_direction: Vector3 = get_gravity().normalized()
 	var downhill_direction: Vector3 = gravity_direction.slide(wall_normal).normalized()
@@ -60,25 +66,27 @@ func _slide_velocity(delta: float) -> void:
 	
 	# Apply acceleration to velocity
 	velocity += z_acceleration * delta
+	print(velocity)
 	
 	# Apply forward drag
-	var deceleration = \
-		drag * velocity.project(z_slope_direction).normalized() * delta
-	if velocity.dot(z_slope_direction) > deceleration.length():
-		velocity -= deceleration
-	else:
-		velocity -= velocity.project(z_slope_direction)
+	_apply_drag(forward_drag, z_slope_direction, delta)
+	print(velocity)
 	
 	# Apply side drag
-	var side_deceleration = \
-		side_drag * velocity.project(downhill_direction).normalized() * delta
-	if velocity.dot(downhill_direction) > side_deceleration.length():
-		velocity -= side_deceleration
-	else:
-		velocity -= velocity.project(downhill_direction)
+	var x_direction: Vector3 = (global_basis.x).normalized()
+	var x_slope_direction: Vector3 = x_direction.slide(wall_normal).normalized()
+	_apply_drag(side_drag, x_slope_direction, delta)
 
 	if velocity.length() > max_speed:
 		velocity = velocity.normalized() * max_speed
+
+func _apply_drag(drag:float, dir: Vector3, delta: float):
+	var deceleration = \
+		drag * velocity.project(dir).normalized() * delta
+	if abs(velocity.dot(dir)) > deceleration.length():
+		velocity -= deceleration
+	else:
+		velocity -= velocity.project(dir)
 
 func _jump(delta: float) -> void:
 	if Input.is_action_pressed("charge_jump"):
@@ -92,16 +100,3 @@ func _jump(delta: float) -> void:
 			if abs(velocity.z) < min_speed:
 				velocity.z -= max_jump_velocity_z * (jump_charge / jump_charge_time)
 		jump_charge = 0
-	
-
-func _align_with_surface(xform: Transform3D):
-	var normal = Vector3.ZERO
-	if is_on_floor():
-		normal = get_floor_normal()
-	else:
-		normal = get_wall_normal()
-
-	xform.basis.y = normal
-	xform.basis.x = -xform.basis.z.cross(normal)
-	xform.basis = xform.basis.orthonormalized()
-	return xform
