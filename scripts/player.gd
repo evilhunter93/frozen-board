020e3cafe40d3bdf7bcfa176edd946bc26e1ed84
KOOL93 <KOOL_music@hotmail.com>
2025-12-21 20:42:11 +0100

    fix(physics): changed slopes to walls
    This allows gravity follow-through
    after landing on a slope.
    Might be better ways of doing this?

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,30 +3,31 @@ extends CharacterBody3D
 @export var max_speed := 20
 @export var acceleration := 2.0
 @export var max_fall_speed := 30
-@export var max_jump_velocity_y := 4.5
+@export var max_jump_velocity_y := 10
 @export var max_jump_velocity_z := 2.0
 @export var jump_charge_time := 1.0
 @export var turn_speed := 1.0
 @export var min_speed := 4.0
-@export var drag := 1.0
+@export var drag := 3.0
 
 @onready var character_mesh: MeshInstance3D = $CharacterMesh
 
 const SPEED = 5.0
 var jump_charge := 0.0
-const JUMP_VELOCITY = 4.5
 var was_in_air = true
 
 func _physics_process(delta: float) -> void:
-	var character_form = _align_with_surface(character_mesh.global_transform)
-	character_mesh.global_transform = \
-		character_mesh.global_transform.interpolate_with(character_form, 0.1)
+	if is_on_floor() or is_on_wall():
+		var character_form = _align_with_surface(character_mesh.global_transform)
+		character_mesh.global_transform = \
+			character_mesh.global_transform.interpolate_with(character_form, 0.1)
+
 	# Add the gravity and handle acceleration
-	if not is_on_floor():
+	if not is_on_floor() and not is_on_wall():
 		was_in_air = true
 		velocity += get_gravity() * delta
 		
-	if is_on_floor():
+	if is_on_wall():
 		_slide_velocity(delta)
 	
 
@@ -36,23 +37,26 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	_jump(delta)
-
+	
 	move_and_slide()
 
 func _slide_velocity(delta: float) -> void:
-	var floor_normal = get_floor_normal()
+	var wall_normal = get_wall_normal()
 
 	if was_in_air:
 		was_in_air = false
-		velocity = velocity.slide(floor_normal)
+		print(velocity)
+		velocity = velocity.slide(wall_normal)
+		print(wall_normal)
+		print(velocity)
 
 	var gravity_direction: Vector3 = get_gravity().normalized()
-	var downhill_direction: Vector3 = gravity_direction.slide(floor_normal).normalized()
+	var downhill_direction: Vector3 = gravity_direction.slide(wall_normal).normalized()
 	# Calculate acceleration along the slope
 	var slope_acceleration: Vector3 = downhill_direction * get_gravity().length()
 	
 	var z_direction: Vector3 = (-global_basis.z).normalized()
-	var z_slope_direction: Vector3 = z_direction.slide(floor_normal).normalized()
+	var z_slope_direction: Vector3 = z_direction.slide(wall_normal).normalized()
 	var z_acceleration: Vector3 = slope_acceleration.project(z_slope_direction)
 	
 	# Apply acceleration to velocity
@@ -83,10 +87,8 @@ func _align_with_surface(xform: Transform3D):
 	var normal = Vector3.ZERO
 	if is_on_floor():
 		normal = get_floor_normal()
-	elif is_on_wall():
-		normal = get_wall_normal()
 	else:
-		normal = Vector3.UP
+		normal = get_wall_normal()
 
 	xform.basis.y = normal
 	xform.basis.x = -xform.basis.z.cross(normal)
